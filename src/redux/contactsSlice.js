d6e5f5543f6eb7bf781a03c9ea4ed19f0e6eab4d
MMolik <molikos@o2.pl>
2024-05-26 21:55:04 +0200

    final commit

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,22 +1,40 @@
+// contactsSlice.js
+
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// Ustawienie bazowego URL dla axios
 axios.defaults.baseURL = 'https://664faeadec9b4a4a602f91b7.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
+  try {
+    console.log('Fetching contacts from: ', axios.defaults.baseURL + '/contacts');
+    const response = await axios.get('/contacts');
+    return response.data;
+  } catch (error) {
+    console.error('Fetch contacts error: ', error);
+    throw error;
+  }
 });
 
 export const addContact = createAsyncThunk('contacts/addContact', async (contact) => {
-  const response = await axios.post('/contacts', contact);
-  return response.data;
+  try {
+    const response = await axios.post('/contacts', contact);
+    return response.data;
+  } catch (error) {
+    console.error('Add contact error: ', error);
+    throw error;
+  }
 });
 
 export const deleteContact = createAsyncThunk('contacts/deleteContact', async (contactId) => {
-  await axios.delete(`/contacts/${contactId}`);
-  return contactId;
+  try {
+    console.log('Deleting contact with id: ', contactId);
+    await axios.delete(`/contacts/${contactId}`);
+    return contactId;
+  } catch (error) {
+    console.error('Delete contact error: ', error);
+    throw error;
+  }
 });
 
 const contactsSlice = createSlice({
--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,10 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+// filterSlice.js
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://664faeadec9b4a4a602f91b7.mockapi.io/';
+
+export const fetchFilters = createAsyncThunk('filters/fetchFilters', async () => {
+  try {
+    console.log('Fetching filters from: ', axios.defaults.baseURL + '/filters');
+    const response = await axios.get('/filters');
+    return response.data;
+  } catch (error) {
+    console.error('Fetch filters error: ', error);
+    throw error;
+  }
+});
 
 const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
   reducers: {
     setFilter: (state, action) => action.payload,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFilters.pending, (state) => {
+        // Tutaj możesz dodać odpowiednie akcje, gdy żądanie jest w trakcie wykonywania
+      })
+      .addCase(fetchFilters.fulfilled, (state, action) => {
+        // Tutaj możesz dodać odpowiednie akcje, gdy żądanie zakończyło się sukcesem
+      })
+      .addCase(fetchFilters.rejected, (state) => {
+        // Tutaj możesz dodać odpowiednie akcje, gdy żądanie zakończyło się niepowodzeniem
+      });
   },
 });
 
